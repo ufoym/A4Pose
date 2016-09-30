0ff8708279df4b49b8b3658c78a3d4f8e15e7eff
Ming <a@ufoym.com>
2016-09-30 16:08:51 +0800

    Add calibration

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,14 @@ int main()
 	// settings
 
 	cv::Size frame_size(1280, 720);
+	const float aspect_ratio = 1.0f;
 	const int board_pad = 100;
 	const int squares_x = 5;
 	const int squares_y = 8;
 	const float square_length = 0.04f;
 	const float marker_length = 0.02f;
 	const int dictionary_id = cv::aruco::DICT_6X6_250;
+	const std::string filename = "camera.yml";
 
 	// ------------------------------------------------------------------------
 	// make board
@@ -22,15 +24,16 @@ int main()
 		cv::aruco::getPredefinedDictionary(
 		cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionary_id));
 
-	cv::Ptr<cv::aruco::CharucoBoard> board =
+	cv::Ptr<cv::aruco::CharucoBoard> ch_board =
 		cv::aruco::CharucoBoard::create(
-		squares_x, squares_y, square_length, marker_length, dictionary);
+		squares_x, squares_y, square_length, marker_length, dictionary); 
+	cv::Ptr<cv::aruco::Board> board = ch_board.staticCast<cv::aruco::Board>();
 
 	// ------------------------------------------------------------------------
 	// make board image for print
 
 	cv::Mat board_img;
-	board->draw(cv::Size(2480, 3508), board_img, board_pad);
+	ch_board->draw(cv::Size(2480, 3508), board_img, board_pad);
 	cv::imwrite("board.png", board_img);
 
 	// ------------------------------------------------------------------------
@@ -57,8 +60,7 @@ int main()
 		std::vector< std::vector< cv::Point2f > > corners, rejected;
 		cv::aruco::detectMarkers(frame, dictionary, corners, ids,
 			cv::aruco::DetectorParameters::create(), rejected);
-		cv::aruco::refineDetectedMarkers(frame,
-			board.staticCast<cv::aruco::Board>(), corners, ids, rejected);
+		cv::aruco::refineDetectedMarkers(frame, board, corners, ids, rejected);
 
 		cv::Mat vis;
 		frame.copyTo(vis);
@@ -68,8 +70,8 @@ int main()
 		}
 		cv::putText(vis,
 			"Press 'c' to add current frame. 'ESC' to finish and calibrate",
-			cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5,
-			cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
+			cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.4,
+			cv::Scalar(255, 255, 0), 1, cv::LINE_AA);
 		cv::imshow("vis", vis);
 
 		const char key = cv::waitKey(30);
@@ -77,11 +79,89 @@ int main()
 			break;
 		}
 		else if (key == 'c' && ids.size() > 0) {
-			std::cout << "Frame captured" << std::endl;
 			all_corners.push_back(corners);
 			all_ids.push_back(ids);
 			all_imgs.push_back(frame);
+			std::cout << "Frame captured #" << all_imgs.size() << std::endl;
 		}
 	}
+
+	// ------------------------------------------------------------------------
+	// camera calibration
+
+	if (all_ids.size() < 1) {
+		std::cerr << "Not enough captures for calibration" << std::endl;
+		return 0;
+	}
+	cv::Mat cameraMatrix, distCoeffs;
+	std::vector< cv::Mat > rvecs, tvecs; 
+	
+	// prepare data for calibration
+	std::vector< std::vector< cv::Point2f > > allCornersConcatenated;
+	std::vector< int > allIdsConcatenated;
+	std::vector< int > markerCounterPerFrame;
+	markerCounterPerFrame.reserve(all_corners.size());
+	for (unsigned int i = 0; i < all_corners.size(); i++) {
+		markerCounterPerFrame.push_back((int)all_corners[i].size());
+		for (unsigned int j = 0; j < all_corners[i].size(); j++) {
+			allCornersConcatenated.push_back(all_corners[i][j]);
+			allIdsConcatenated.push_back(all_ids[i][j]);
+		}
+	}
+
+	double arucoRepErr = cv::aruco::calibrateCameraAruco(
+		allCornersConcatenated, allIdsConcatenated,
+		markerCounterPerFrame, board, frame_size, 
+		cameraMatrix, distCoeffs);
+
+	int nFrames = (int)all_corners.size();
+	std::vector< cv::Mat > allCharucoCorners;
+	std::vector< cv::Mat > allCharucoIds;
+	std::vector< cv::Mat > filteredImages;
+	allCharucoCorners.reserve(nFrames);
+	allCharucoIds.reserve(nFrames);
+
+	for (int i = 0; i < nFrames; i++) {
+		// interpolate using camera parameters
+		cv::Mat currentCharucoCorners, currentCharucoIds;
+		cv::aruco::interpolateCornersCharuco(
+			all_corners[i], all_ids[i], all_imgs[i], ch_board,
+			currentCharucoCorners, currentCharucoIds, 
+			cameraMatrix, distCoeffs);
+
+		allCharucoCorners.push_back(currentCharucoCorners);
+		allCharucoIds.push_back(currentCharucoIds);
+		filteredImages.push_back(all_imgs[i]);
+	}
+
+	if (allCharucoCorners.size() < 4) {
+		std::cerr << "Not enough corners for calibration" << std::endl;
+		return 0;
+	}
+
+	double repError =
+		cv::aruco::calibrateCameraCharuco(
+		allCharucoCorners, allCharucoIds, ch_board, frame_size,
+		cameraMatrix, distCoeffs, rvecs, tvecs, 0);
+	
+	// ------------------------------------------------------------------------
+	// save camera parameters
+
+	cv::FileStorage fs(filename, cv::FileStorage::WRITE);
+	if (!fs.isOpened()) {
+		std::cerr << "Cannot save output file" << std::endl;
+		return 0;
+	}
+	fs << "image_width" << frame_size.width;
+	fs << "image_height" << frame_size.height;
+	fs << "camera_matrix" << cameraMatrix;
+	fs << "distortion_coefficients" << distCoeffs;
+	fs << "avg_reprojection_error" << repError;
+
+
+	std::cout << "Rep Error: " << repError << std::endl;
+	std::cout << "Rep Error Aruco: " << arucoRepErr << std::endl;
+	std::cout << "Calibration saved to " << filename << std::endl;
+
 	return 0;
 }
